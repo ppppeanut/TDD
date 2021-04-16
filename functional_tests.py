28ab631b31285fb3da4ccfedc5454c4630c9ddfc
Your Name <you@example.com>
2021-04-16 15:05:55 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,10 +18,10 @@ class NewVisitorTest(unittest.TestCase):        #(1)
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', head_text)
+        self.assertIn('To-Do', header_text)
 
         #She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_elemrnt_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
--- lists/tests.py
+++ lists/tests.py
@@ -8,6 +8,11 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
+        html = response.content.decode('utf8') #3
+        self.assertTrue(html.startswith('<html>')) #4
+        self.assertIn('<title>To-Do lists</title>', html) #5
+        self.assertTrue(html.endswith('</html>')) #4
+        
         self.assertTemplateUsed(response, 'home.html')        
 
 # Create your tests here.
