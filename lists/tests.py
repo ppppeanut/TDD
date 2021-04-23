5da86a4ef23dafadb0468d6eaac00f5dbd2b1e1e
Your Name <you@example.com>
2021-04-23 14:20:43 +0800

    A table item is finished

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,21 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page
+#from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
-        html = response.content.decode('utf8') #3
-        self.assertTrue(html.startswith('<html>')) #4
-        self.assertIn('<title>To-Do lists</title>', html) #5
-        self.assertTrue(html.endswith('</html>')) #4
         
-        self.assertTemplateUsed(response, 'home.html')        
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+        
 
 # Create your tests here.
 
