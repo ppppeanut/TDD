aa0e3940620a66cebdda726a4922bc62d9ab8ff6
Your Name <you@example.com>
2021-05-21 13:57:55 +0800

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -73,15 +73,7 @@ class NewVisitorTest(LiveServerTestCase):        #(1)
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-
-##    def test_can_start_a_list_for_one_user(self):
-##        #Edith has heard about a cool new online to-do app. She goes
-##
-##        #The page updates again, and now shows both items on her list
-##        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-##        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-##
-##        #Satisfied, she goes back to sleep
+                
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
         #Edith starts a new to-do list
@@ -93,14 +85,14 @@ class NewVisitorTest(LiveServerTestCase):        #(1)
 
         #She notices that her list has a unique URL
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/list/.+')
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         #Now a new user, Francis, coms along to the site.
 
         ##We use a new browser session to make sure that no information
         ##of Edith's is coming through from cookies etc
         self.browser.quit()
-        self.browser = webdriver.Firxfox()
+        self.browser = webdriver.Firefox()
 
         #Francis visits the home page. There is no sign of Edith's list
         self.browser.get(self.live_server_url)
